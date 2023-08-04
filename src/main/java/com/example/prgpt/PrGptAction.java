22c28dd95d4fef7dd2aedfad754bee218ec080b6
Imvikram99 <120546792+Imvikram99@users.noreply.github.com>
2023-08-04 15:31:05 +0530

    working code , able to fetch all the changes file wise

--- src/main/java/com/example/prgpt/PrGptAction.java
+++ src/main/java/com/example/prgpt/PrGptAction.java
@@ -2,7 +2,6 @@ package com.example.prgpt;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.Change;
@@ -12,48 +11,39 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
 
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class PrGptAction extends AnAction {
+    private static Integer lineToIncludeAboveChange = 3;
+    private static Integer lineToIncludeBelowChange = 2;
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getProject();
-
-        // Get the ChangeListManager instance for the current project
         ChangeListManager changeListManager = ChangeListManager.getInstance(project);
-
-        // Get all local changes
         List<Change> changes = (List<Change>) changeListManager.getAllChanges();
 
-        // Apply uppercase transformation to the changes
         try {
-            makeChangesUpperCase(changes, project);
+            Map<String, Map<Integer, List<String>>> allChanges = getListOfChangedCodePara(changes, project);
+            // Additional code...
+            int k = 0;
         } catch (VcsException ex) {
             throw new RuntimeException(ex);
         }
     }
 
-    private void makeChangesUpperCase(List<Change> changes, Project project) throws VcsException {
+    private Map<String, Map<Integer, List<String>>> getListOfChangedCodePara(List<Change> changes, Project project) throws VcsException {
+        Map<String, Map<Integer, List<String>>> fileNameToChanges = new LinkedHashMap<>();
         for (Change change : changes) {
-            // Get the 'after' revision
             ContentRevision afterRevision = change.getAfterRevision();
             ContentRevision beforeRevision = change.getBeforeRevision();
-
-            // Get the file path
             com.intellij.openapi.vcs.FilePath filePath = afterRevision.getFile();
 
-            // Check if the file is inside the src folder
-            if (!filePath.getPath().contains("/src/")) {
-                continue; // Skip if not inside src folder
-            }
+            if (!filePath.getPath().contains("/src/")) continue;
 
-            // Convert FilePath to VirtualFile
             VirtualFile virtualFile = filePath.getVirtualFile();
-
-            // Read the file content
             String originalContent;
+
             try {
                 originalContent = new String(virtualFile.contentsToByteArray(), virtualFile.getCharset());
             } catch (IOException ex) {
@@ -61,45 +51,52 @@ public class PrGptAction extends AnAction {
                 continue;
             }
 
-            // Get before and after content for the diff
             String beforeContent = beforeRevision != null ? beforeRevision.getContent() : "";
             String afterContent = afterRevision != null ? afterRevision.getContent() : "";
+            LinkedHashMap<Integer, List<String>> listOfChangesLineWise = getListOfChanges(beforeContent, afterContent);
 
-            // Apply uppercase transformation to the changes
-            String transformedContent = applyUppercaseToChanges(beforeContent, afterContent);
-
-            // Write the transformed content back to the file
-            WriteAction.run(() -> {
-                try {
-                    virtualFile.setBinaryContent(transformedContent.getBytes(virtualFile.getCharset()));
-                } catch (IOException ex) {
-                    System.out.println("Error writing to file: " + filePath.getPath());
-                }
-            });
+            fileNameToChanges.put(filePath.getName(), listOfChangesLineWise);
         }
+
+        return fileNameToChanges;
     }
 
-    private String applyUppercaseToChanges(String beforeContent, String afterContent) {
+    private LinkedHashMap<Integer, List<String>> getListOfChanges(String beforeContent, String afterContent) {
         DiffMatchPatch dmp = new DiffMatchPatch();
         LinkedList<DiffMatchPatch.Diff> diffs = dmp.diffMain(beforeContent, afterContent);
-
-        StringBuilder modifiedContent = new StringBuilder();
-        int currentIndex = 0;
+        LinkedHashMap<Integer, List<String>> changes = new LinkedHashMap<>();
+        int currentLineNo = 0;
+        String[] lines = afterContent.split("\n");
 
         for (DiffMatchPatch.Diff diff : diffs) {
-            switch (diff.operation) {
-                case EQUAL:
-                    modifiedContent.append(afterContent.substring(currentIndex, currentIndex + diff.text.length()));
-                    break;
-                case DELETE:
-                    break;
-                case INSERT:
-                    modifiedContent.append(diff.text.toUpperCase());
-                    break;
+            if (diff.operation == DiffMatchPatch.Operation.INSERT) {
+                int lineEnd = currentLineNo;
+                int prevLineStart = lineEnd - lineToIncludeAboveChange;
+                if (prevLineStart < 0) prevLineStart = 0;
+                int changeStartLineNo = lineEnd - lineToIncludeAboveChange;
+                if (changeStartLineNo < 0) changeStartLineNo = 0;
+                int nextLineEnd = lineEnd + lineToIncludeBelowChange;
+                List<String> paragraph = new ArrayList<>();
+                for (int i = prevLineStart; i <= nextLineEnd && i < lines.length; i++) {
+                    paragraph.add(lines[i]);
+                }
+                changes.put(changeStartLineNo, paragraph);
+                currentLineNo += countNewlines(diff.text);
+            } else if (diff.operation == DiffMatchPatch.Operation.EQUAL) {
+                currentLineNo += countNewlines(diff.text);
             }
-            currentIndex += diff.text.length();
         }
 
-        return modifiedContent.toString();
+        return changes;
+    }
+
+    private int countNewlines(String text) {
+        int count = 0;
+        for (char c : text.toCharArray()) {
+            if (c == '\n') {
+                count++;
+            }
+        }
+        return count;
     }
 }
